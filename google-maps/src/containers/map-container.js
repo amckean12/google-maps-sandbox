f41190802d9fe633ee086e23609c764a61d1e9d2
amckean12 <amckean@me.com>
2019-07-12 14:07:54 -0500

    initial setup to start map implementation

---
+++ google-maps/src/containers/map-container.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import '../css/map-styling.css'
+
+class MapContainer extends Component {
+  render(){
+    return(
+      <div className="map-container">
+      
+      </div>
+    )
+  }
+}
+
+export default MapContainer
