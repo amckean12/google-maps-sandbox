9393c8275cc92d196a51c8de8eb62d54b2c52950
amckean12 <amckean@me.com>
2019-07-13 13:29:52 -0500

    removed react map packages

--- google-maps/src/containers/map-container.js
+++ google-maps/src/containers/map-container.js
@@ -2,7 +2,7 @@
 import React, { Component } from 'react'
 
 //Components
-import MapComponent from '../components/map-component.js'
+
 
 //UI Assets
 import '../css/map-styling.css'
@@ -11,13 +11,6 @@ class MapContainer extends Component {
   render(){
     return(
       <div className="map-container">
-      <MapComponent
-        isMarkerShown
-        googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
-        loadingElement={<div style={{ height: `100%` }} />}
-        containerElement={<div style={{ height: `100%` }} />}
-        mapElement={<div style={{ height: `100%` }} />}
-      />
       </div>
     )
   }
